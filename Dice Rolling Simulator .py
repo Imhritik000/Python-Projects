2f3f0ce6e871c34764c140464cf629e0424652d1
Imhritik000 <hritikch000@gmail.com>
2025-04-23 10:35:45 +0530

    Add files via upload

---
+++ Dice Rolling Simulator .py
@@ -0,0 +1,25 @@
+                                       #Dice Rolling Simulator 🎲
+
+"""Goal:Generate and display a random number between 1 and 6 each
+         time the user rolls the dice ."""
+
+
+
+import random
+
+
+def roll_dice():
+    return random.randint(1, 6)
+
+
+print("Dice Rolling Simulator 🎲")
+
+while True:
+    roll = input("Roll the dice? (yes/no): ").lower()
+    if roll == "yes":
+        print("You rolled:", roll_dice())
+    elif roll == "no":
+        print("Thanks for playing!")
+        break
+    else:
+        print("Please type 'yes' or 'no'")
